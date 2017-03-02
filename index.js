99d4bcdebcc7057013d421d784718b4a7e1ebc8e
Michael Trim <michael.trim@connectib.com>
2017-03-02 13:05:54 +0000

    stop event propagation on click

--- index.js
+++ index.js
@@ -115,6 +115,8 @@ class Dropdown extends Component {
     if (this.mounted) {
       if (!ReactDOM.findDOMNode(this).contains(event.target)) {
         this.setState({ isOpen: false })
+        event.stopPropagation();
+        event.preventDefault();
       }
     }
   }
